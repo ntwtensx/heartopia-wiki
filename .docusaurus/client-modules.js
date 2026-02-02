e8d47e2fb606e4d70548947cc880a2917ac0d7d0
ntwtensx <ntw205biz@gmail.com>
2026-02-03 06:28:19 +0700

    Deploy website - based on 9449b346667b65ff87b8d4fca185fec79e621c4e

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\waran.MIROSALVA\\heartopia-wiki\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\waran.MIROSALVA\\heartopia-wiki\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\waran.MIROSALVA\\heartopia-wiki\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\waran.MIROSALVA\\heartopia-wiki\\src\\css\\custom.css"),
-];
